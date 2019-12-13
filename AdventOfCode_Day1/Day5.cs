4b46800ea4fd5ab6c520a0529d3bc69d64e3c9dd
ginisth <ginisth@hotmail.com>
2019-12-13 17:33:03 +0200

    Day7-Part1

--- AdventOfCode_Day1/Day5.cs
+++ AdventOfCode_Day1/Day5.cs
@@ -16,60 +16,72 @@ namespace AdventOfCode2019
             List<int> input = FileLines[0].Split(',').Select(int.Parse).ToList();
             int part1 = Intcode(input, true);
             int part2 = Intcode(input, false);
+
             Console.WriteLine("Part1: " + part1);
             Console.WriteLine("Part2: " + part2);
         }
 
-        public static int Intcode(List<int> input, bool part1)
+        public int Intcode(List<int> input, bool part1, int? Day7_PhaseSetting = null, int? Day7_Input = null)
         {
             List<int> numbers = input.ToList();
             int count = 0;
-            while (numbers[count] != 99)
-            {
-                int number = numbers[count];
-                int opCode = number % 10;
-                int parameter1Mode = (number / 100) % 10;
-                int parameter2Mode = (number / 1000) % 10;
-                switch (opCode)
-                {
-                    case 1:
-                    case 2:
-                    case 5:
-                    case 6:
-                    case 7:
-                    case 8:
-                        int param1 = parameter1Mode == 1 ? numbers[count + 1] : numbers[numbers[count + 1]];
-                        int param2 = parameter2Mode == 1 ? numbers[count + 2] : numbers[numbers[count + 2]];
-                        int address = numbers[count + 3];
-                        int result = Opcode(opCode, param1, param2, count);
+            bool Day7_firstInstruction = true;
 
-                        if (opCode != 5 && opCode != 6)
-                        {
-                            numbers[address] = result;
-                            count += 4;
-                        }
-                        else
-                            count = result;
-                        break;
-                    case 3:
-                        //input
-                        int case3param = numbers[count + 1];
-                        numbers[case3param] = part1 ? 1 : 5;
-                        count += 2;
-                        break;
-                    case 4:
-                        if (numbers[count + 2] == 99)
-                        {
-                            int case4Addr = numbers[count + 1];
-                            return numbers[case4Addr];
-                        }
-                        count += 2;
-                        break;
-                    default:
-                        break;
+            {
+                while (numbers[count] != 99)
+                {
+                    int number = numbers[count];
+                    int opCode = number % 10;
+                    int parameter1Mode = (number / 100) % 10;
+                    int parameter2Mode = (number / 1000) % 10;
+                    switch (opCode)
+                    {
+                        case 1:
+                        case 2:
+                        case 5:
+                        case 6:
+                        case 7:
+                        case 8:
+                            int param1 = parameter1Mode == 1 ? numbers[count + 1] : numbers[numbers[count + 1]];
+                            int param2 = parameter2Mode == 1 ? numbers[count + 2] : numbers[numbers[count + 2]];
+                            int address = numbers[count + 3];
+                            int result = Opcode(opCode, param1, param2, count);
+
+                            if (opCode != 5 && opCode != 6)
+                            {
+                                numbers[address] = result;
+                                count += 4;
+                            }
+                            else
+                                count = result;
+                            break;
+                        case 3:
+                            //input
+                            int case3param = numbers[count + 1];
+                            if (Day7_PhaseSetting != null)
+                            {
+                                numbers[case3param] = AmplifierController(Day7_firstInstruction, (int)Day7_PhaseSetting, (int)Day7_Input);
+                                Day7_firstInstruction = false;
+                            }
+                            else
+                                numbers[case3param] = part1 ? 1 : 5;
+                            count += 2;
+                            break;
+                        case 4:
+                            if (numbers[count + 2] == 99)
+                            {
+                                int case4Addr = numbers[count + 1];
+                                return numbers[case4Addr];
+                            }
+                            count += 2;
+                            break;
+                        default:
+                            break;
+                    }
                 }
+
+                return numbers[0];
             }
-            return numbers[0];
         }
 
 
@@ -93,6 +105,11 @@ namespace AdventOfCode2019
                 default:
                     throw new Exception();
             }
+        }
+
+        private static int AmplifierController(bool firstInstuction, int phaseSetting, int input)
+        {
+            return firstInstuction ? phaseSetting : input;
         }
     }
 }
---
+++ AdventOfCode_Day1/Day7.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2019
+{
+    public class Day7 : AbstractDay
+    {
+        public Day7(string[] fileLines) : base(fileLines) { }
+
+
+        public override void MainCalculation()
+        {
+            List<int> inputList = FileLines[0].Split(',').Select(int.Parse).ToList();
+            List<int> finalSignals = new List<int>();
+
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j < 5; j++)
+                {
+                    for (int k = 0; k < 5; k++)
+                    {
+                        for (int p = 0; p < 5; p++)
+                        {
+                            for (int l = 0; l < 5; l++)
+                            {
+                                int[] phaseSettings = new int[] { i, j, k, p, l };
+
+                                if (phaseSettings.Count() == phaseSettings.Distinct().Count())
+                                {
+                                    Day5 day5 = new Day5(FileLines);
+                                    int outputAmplifier1 = day5.Intcode(inputList, false, i, 0);
+                                    int outputAmplifier2 = day5.Intcode(inputList, false, j, outputAmplifier1);
+                                    int outputAmplifier3 = day5.Intcode(inputList, false, k, outputAmplifier2);
+                                    int outputAmplifier4 = day5.Intcode(inputList, false, p, outputAmplifier3);
+                                    int outputAmplifier5 = day5.Intcode(inputList, false, l, outputAmplifier4);
+                                    finalSignals.Add(outputAmplifier5);
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+
+            Console.WriteLine("Highest Signal: " + finalSignals.Max());
+        }
+    }
+}
